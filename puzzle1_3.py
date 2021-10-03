b2a6bb631ecd676dee5c04118e3042f3a141e475
FabreAlb <90712814+FabreAlb@users.noreply.github.com>
2021-10-03 11:25:48 +0200

    Create puzzle1_3.py

---
+++ puzzle1_3.py
@@ -0,0 +1,20 @@
+from pynfc import Nfc, Desfire, Timeout
+
+class Rfid:
+    
+    
+    def __init__(self):
+        self.nfc = Nfc("pn532_uart:/dev/ttyS0:115200")
+        
+    # return uid in hexa str 
+    def read_uid(self):
+        n = Nfc("pn532_uart:/dev/ttyS0")
+        for target in n.poll():
+            hexa=target.uid.decode(encoding='utf-8')
+            return hexa.upper()
+        
+if __name__ == "__main__":
+
+    rf = Rfid()
+    uid = rf.read_uid()
+    print(uid)
